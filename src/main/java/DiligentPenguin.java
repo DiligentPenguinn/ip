905ab49893adc16f498baeb6faafc63a97f2af44
Dang Hoang Duy <e1129930@u.nus.edu>
2025-01-23 13:15:11 +0800

    add methos store and list, make use of MyList to store and print items

--- src/main/java/DiligentPenguin.java
+++ src/main/java/DiligentPenguin.java
@@ -2,6 +2,7 @@ import java.util.Objects;
 import java.util.Scanner;
 
 public class DiligentPenguin {
+    static MyList list = new MyList();
     static String horizontalLines = "-----------------------------------------------";
     static String name = "DiligentPenguin";
     public static void greet() {
@@ -11,12 +12,29 @@ public class DiligentPenguin {
     }
 
     public static void exit() {
+        System.out.println(horizontalLines);
         System.out.println("Bye bye. Come back to me soon!");
         System.out.println(horizontalLines);
     }
 
     public static void echo(String userInput) {
+        System.out.println(horizontalLines);
         System.out.println(userInput);
+        System.out.println(horizontalLines);
+    }
+
+    public static void store(String userInput) {
+        System.out.println(horizontalLines);
+        System.out.println("Noted. I will write this down for you!");
+        System.out.println(horizontalLines);
+        list.add(userInput);
+    }
+
+    public static void list() {
+        System.out.println(horizontalLines);
+        System.out.println("Here is the list of items I noted down");
+        System.out.println(list.toString());
+        System.out.println(horizontalLines);
     }
 
     public static void main(String[] args) {
@@ -29,9 +47,12 @@ public class DiligentPenguin {
             if (Objects.equals(userInput, "bye")) {
                 DiligentPenguin.exit();
                 break;
+            } else if (Objects.equals(userInput, "list")) {
+                DiligentPenguin.list();
+                continue;
             }
 
-            DiligentPenguin.echo(userInput);
+            DiligentPenguin.store(userInput);
         }
 
     }
