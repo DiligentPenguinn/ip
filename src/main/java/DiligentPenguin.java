5454d175e335c11ba6741cab3463bd3156e9ee3c
Dang Hoang Duy <e1129930@u.nus.edu>
2025-01-23 12:52:06 +0800

    change chatbot's personality

--- src/main/java/DiligentPenguin.java
+++ src/main/java/DiligentPenguin.java
@@ -6,12 +6,12 @@ public class DiligentPenguin {
     static String name = "DiligentPenguin";
     public static void greet() {
         System.out.println(horizontalLines);
-        System.out.printf("Hello! I'm %s \nWhat can I do for you? \n", name);
+        System.out.printf("Hello there! My name is %s \nTell me what you want to do! \n", name);
         System.out.println(horizontalLines);
     }
 
     public static void exit() {
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("Bye bye. Come back to me soon!");
         System.out.println(horizontalLines);
     }
 
