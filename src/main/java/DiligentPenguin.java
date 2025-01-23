ec0a6f4d7b9f3c282eea7947ee019f9af85a27c7
Dang Hoang Duy <e1129930@u.nus.edu>
2025-01-23 12:39:20 +0800

    Change chatbot's name and add a greet method

---
+++ src/main/java/DiligentPenguin.java
@@ -0,0 +1,12 @@
+public class DiligentPenguin {
+    static String horizontalLines = "-----------------------------------------------";
+    static String name = "DiligentPenguin";
+    public static void greet() {
+        System.out.println(horizontalLines);
+        System.out.printf("Hello! I'm %s \nWhat can I do for you? \n", name);
+        System.out.println(horizontalLines);
+    }
+    public static void main(String[] args) {
+        DiligentPenguin.greet();
+    }
+}
