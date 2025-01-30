6ddb5c5492a66188a036fa400b79a7ee6a917d44
Dang Hoang Duy <e1129930@u.nus.edu>
2025-01-30 17:01:21 +0800

    Add JavaDoc comments to remaining classes/methods

--- src/main/java/diligentpenguin/ChatBotException.java
+++ src/main/java/diligentpenguin/ChatBotException.java
@@ -1,4 +1,9 @@
 package diligentpenguin;
+
+/**
+ * Customized exception for the chatbot.
+ * This exception is thrown when the chatbot encounters an invalid command or input.
+ */
 public class ChatBotException extends Exception {
     public ChatBotException(String message) {
         super(message);
