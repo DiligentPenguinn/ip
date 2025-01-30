0a90729215032d63523546eae8f1be814999197c
Dang Hoang Duy <e1129930@u.nus.edu>
2025-01-30 12:42:28 +0800

    Divide classes into packages

---
+++ src/main/java/diligentpenguin/task/Task.java
@@ -0,0 +1,35 @@
+package diligentpenguin.task;
+import java.time.format.DateTimeFormatter;
+
+public class Task {
+    public static DateTimeFormatter inputFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+    static DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("EEEE, MMMM d, yyyy");
+    String name = "";
+    String type = "";
+    Boolean isDone = false;
+    public Task(String name) {
+        this.name = name;
+    }
+
+    public void setDone() {
+        this.isDone = true;
+    }
+
+    public void setUnDone() {
+        this.isDone = false;
+    }
+
+    public String getType() {
+        return this.type;
+    }
+
+    @Override
+    public String toString() {
+        String mark = this.isDone ? "X" : " ";
+        return String.format("[%s] %s", mark, this.name);
+    }
+
+    public String toSavedString() {
+        return "";
+    }
+}
---
+++ src/main/java/diligentpenguin/task/ToDo.java
@@ -0,0 +1,19 @@
+package diligentpenguin.task;
+public class ToDo extends Task {
+    public ToDo(String name) {
+        super(name);
+        this.type = "T";
+    }
+
+    @Override
+    public String toString() {
+        String mark = this.isDone ? "X" : " ";
+        return String.format("[%s][%s] %s", this.type, mark, this.name);
+    }
+
+    @Override
+    public String toSavedString() {
+        String mark = this.isDone ? "X" : " ";
+        return String.format("%s | %s | %s", this.type, mark, this.name);
+    }
+}
