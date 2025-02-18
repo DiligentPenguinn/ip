9f6f847ee0107fdb4ba16493ffc34a1ac2dc751f
Dang Hoang Duy <e1129930@u.nus.edu>
2025-02-18 12:06:37 +0800

    Fix style

--- src/main/java/diligentpenguin/exception/BadDateTimeException.java
+++ src/main/java/diligentpenguin/exception/BadDateTimeException.java
@@ -1,5 +1,8 @@
 package diligentpenguin.exception;
 
+/**
+ * Represents exception where start time is after end time
+ */
 public class BadDateTimeException extends ChatBotException {
     private static final String MESSAGE = "Your end date cannot be before your start date!";
 
